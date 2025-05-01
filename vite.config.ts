7fbbc49d30326f1c358692a682784002892d5507
Line Son <wkdnfla14@gmail.com>
2025-05-01 16:59:36 +0900

    fix: Update vite.config.ts base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/linekiller89.github.io/", // GitHub Pages 배포를 위한 base 경로
+  base: "/", // GitHub Pages 사용자 페이지 배포를 위한 base 경로
 });
